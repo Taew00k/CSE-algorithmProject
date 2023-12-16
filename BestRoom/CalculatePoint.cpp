4f2f8d1596f37f30628b4445808d8a923a91b2a4
taewook <davis0625@dongguk.edu>
2023-12-17 04:03:56 +0900

    Add: main과 calculatePoint 합치기

--- BestRoom/CalculatePoint.cpp
+++ BestRoom/CalculatePoint.cpp
@@ -1,3 +1,29 @@
+// CalculatePoint.cpp
 #include "CalculatePoint.h"
-#inlcude <iostream>
+
+MinMax findMinMax(std::vector<House> houses, double House::* feature) {
+    MinMax result{std::numeric_limits<float>::max(), std::numeric_limits<float>::min()};
+    for (const auto& house : houses) {
+        if (house.*feature < result.min) result.min = house.*feature;
+        if (house.*feature > result.max) result.max = house.*feature;
+    }
+    return result;
+}
+
+std::vector<House> calculateScores(std::vector<House> houses, int monthlyWeight, int depositWeight, int distanceWeight, int sizeWeight) {
+    MinMax monthlyMM = findMinMax(houses, &House::monthly);
+    MinMax depositMM = findMinMax(houses, &House::deposit);
+    MinMax distanceMM = findMinMax(houses, &House::distance);
+    MinMax sizeMM = findMinMax(houses, &House::size);
+
+    for (auto& house : houses) {
+        house.totalScore = ((1 - (house.monthly - monthlyMM.min) / (monthlyMM.max - monthlyMM.min)) * monthlyWeight +
+                            (1-(house.deposit - depositMM.min) / (depositMM.max - depositMM.min)) * depositWeight +
+                            (1-(house.distance - distanceMM.min) / (distanceMM.max - distanceMM.min)) * distanceWeight +
+                            (house.size - sizeMM.min) / (sizeMM.max - sizeMM.min) * sizeWeight);
+    }
+    return houses;
+}
+
+
 
