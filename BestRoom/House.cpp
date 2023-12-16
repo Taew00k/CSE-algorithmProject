7d83ebfd602c2da8198d3942b9844699a66820bd
eundeang <111877048+eundeang@users.noreply.github.com>
2023-12-16 22:17:53 +0900

    house 구조체 생성

---
+++ BestRoom/House.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+struct house {
+    string roadNameAddress; //도로명 주소
+    double monthly;     // 월세 + 관리비
+    double deposit;      // 보증금
+    double distance;    // 신공학관으로부터 거리
+    double size;         // 평수
+    double floor;          // 층수
+    bool security; // 공동현관 유무, cctv
+};
+
+
